7ec56fccd1beb7a409f54e7e022d668f439a4df7
George Dobrin <77446421+george-dobrin@users.noreply.github.com>
2021-08-25 21:16:59 +0300

    code refactoring; added LoRaWAN connection

--- main.ino
+++ main.ino
@@ -1,46 +1,30 @@
-/* Heltec Automation Receive communication test example
- *
- * Function:
- * 1. Receive the same frequency band lora signal program
- * 
- * 
- * this project also realess in GitHub:
- * https://github.com/HelTecAutomation/ASR650x-Arduino
- * */
-
 #include "LoRaWan_APP.h"
 #include "Arduino.h"
 
-/*
- * set LoraWan_RGB to 1,the RGB active in loraWan
- * RGB red means sending;
- * RGB green means received done;
- */
 #ifndef LoraWan_RGB
 #define LoraWan_RGB 0
 #endif
 
-#define RF_FREQUENCY                                868000000 // Hz
+#define RF_FREQUENCY 868000000 // Hz
 
-#define TX_OUTPUT_POWER                             14        // dBm
+#define TX_OUTPUT_POWER 14 // dBm
 
-#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
-                                                              //  1: 250 kHz,
-                                                              //  2: 500 kHz,
-                                                              //  3: Reserved]
-#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
-#define LORA_CODINGRATE                             1         // [1: 4/5,
-                                                              //  2: 4/6,
-                                                              //  3: 4/7,
-                                                              //  4: 4/8]
-#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
-#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
-#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
-#define LORA_IQ_INVERSION_ON                        false
+#define LORA_BANDWIDTH 0        // [0: 125 kHz, \
+                                //  1: 250 kHz, \
+                                //  2: 500 kHz, \
+                                //  3: Reserved]
+#define LORA_SPREADING_FACTOR 7 // [SF7..SF12]
+#define LORA_CODINGRATE 1       // [1: 4/5, \
+                                //  2: 4/6, \
+                                //  3: 4/7, \
+                                //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH 8  // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 0   // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
 
-
-#define RX_TIMEOUT_VALUE                            1000
-#define BUFFER_SIZE                                 30 // Define the payload size here
+#define RX_TIMEOUT_VALUE 1000
+#define BUFFER_SIZE 30 // Define the payload size here
 
 char txpacket[BUFFER_SIZE];
 char rxpacket[BUFFER_SIZE];
@@ -49,44 +33,150 @@ static RadioEvents_t RadioEvents;
 
 int16_t txNumber;
 
-int16_t rssi,rxSize;
+int16_t rssi, rxSize;
+
+// LoRaWAN params
+
+// uint8_t devEui[] = {0x22, 0x32, 0x33, 0x00, 0x00, 0x88, 0x88, 0x02};
+uint8_t appEui[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t appKey[] = {0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x66, 0x01};
+
+uint16_t userChannelsMask[6] = {0x00FF, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000};
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+DeviceClass_t loraWanClass = LORAWAN_CLASS;
+uint32_t appTxDutyCycle = 15000;
+bool overTheAirActivation = LORAWAN_NETMODE;
+bool loraWanAdr = LORAWAN_ADR;
+bool keepNet = LORAWAN_NET_RESERVE;
+bool isTxConfirmed = LORAWAN_UPLINKMODE;
+uint8_t appPort = 2;
+uint8_t confirmedNbTrials = 4;
 
 
-void setup() {
-    Serial.begin(115200);
+//messages queue
+char *messagesQueue[10];
+int queueIndex = 0;
 
-    txNumber=0;
-    rssi=0;
-  
-    RadioEvents.RxDone = OnRxDone;
-    Radio.Init( &RadioEvents );
-    Radio.SetChannel( RF_FREQUENCY );
-  
-  Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
-                                   LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
-                                   LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
-                                   0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
-   turnOnRGB(COLOR_SEND,0); //change rgb color
-   Serial.println("into RX mode");
-   }
+static void prepareTxFrame(uint8_t port, int i)
+{
+  /*appData size is LORAWAN_APP_DATA_MAX_SIZE which is defined in "commissioning.h".
+	*appDataSize max value is LORAWAN_APP_DATA_MAX_SIZE.
+	*if enabled AT, don't modify LORAWAN_APP_DATA_MAX_SIZE, it may cause system hanging or failure.
+	*if disabled AT, LORAWAN_APP_DATA_MAX_SIZE can be modified, the max value is reference to lorawan region and SF.
+	*for example, if use REGION_CN470, 
+	*the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
+	*/
 
+  appDataSize = 40;
+  appData = messagesQueue[i];
+}
 
+void sendToTTN()
+{
+
+#if (AT_SUPPORT)
+  enableAt();
+#endif
+  deviceState = DEVICE_STATE_INIT;
+  LoRaWAN.ifskipjoin();
+
+  // send LoRaWAN message
+  switch (deviceState)
+  {
+  case DEVICE_STATE_INIT:
+  {
+#if (LORAWAN_DEVEUI_AUTO)
+    LoRaWAN.generateDeveuiByChipID();
+#endif
+#if (AT_SUPPORT)
+    getDevParam();
+#endif
+    printDevParam();
+    LoRaWAN.init(loraWanClass, loraWanRegion);
+    deviceState = DEVICE_STATE_JOIN;
+    break;
+  }
+  case DEVICE_STATE_JOIN:
+  {
+    LoRaWAN.join();
+    break;
+  }
+  case DEVICE_STATE_SEND:
+  {
+    for(int i=0;i<queueIndex;i++){
+      prepareTxFrame(appPort, i);
+      LoRaWAN.send();
+    }
+    queueIndex = 0;
+    messagesQueue = null;
+    allocateQueueMemory();
+    deviceState = DEVICE_STATE_SLEEP;
+    break;
+  }
+  case DEVICE_STATE_SLEEP:
+  {
+    LoRaWAN.sleep();
+    break;
+  }
+  default:
+  {
+    deviceState = DEVICE_STATE_INIT;
+    break;
+  }
+  }
+}
+
+void setupRxLoRA()
+{
+  txNumber = 0;
+  rssi = 0;
+
+  RadioEvents.RxDone = OnRxDone;
+  Radio.Init(&RadioEvents);
+  Radio.SetChannel(RF_FREQUENCY);
+
+  Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                    LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                    LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
+  turnOnRGB(COLOR_SEND, 0); //change rgb color
+  Serial.println("into RX mode");
+}
+
+void receiveLoRaMessages()
+{
+  Radio.Rx(0);
+  delay(500);
+  Radio.IrqProcess();
+}
+
+void allocateQueueMemory(){
+  for (int i = 0; i < 10; ++i){
+    messagesQueue[i] = (char*)malloc(50);
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  allocateQueueMemory();
+  setupRxLoRA();
+}
 
 void loop()
 {
-  Radio.Rx( 0 );
-  delay(500);
-  Radio.IrqProcess( );
+  receiveLoRaMessages();
 }
 
-void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr)
 {
-    rssi=rssi;
-    rxSize=size;
-    memcpy(rxpacket, payload, size );
-    rxpacket[size]='\0';
-    turnOnRGB(COLOR_RECEIVED,0);
-    Radio.Sleep( );
-    Serial.printf("\r\nreceived packet \"%s\" with rssi %d , length %d\r\n",rxpacket,rssi,rxSize);
-
+  rssi = rssi;
+  rxSize = size;
+  memcpy(rxpacket, payload, size);
+  rxpacket[size] = '\0';
+  messagesQueue[queueIndex] = rxpacket;
+  queueIndex++;
+  turnOnRGB(COLOR_RECEIVED, 0);
+  Radio.Sleep();
+  Serial.printf("\r\nreceived packet \"%s\" with rssi %d , length %d\r\n", rxpacket, rssi, rxSize);
 }
